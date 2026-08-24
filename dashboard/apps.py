cea0f00bc98071708cfc1574f9c996e38c8731a9
cyw0927 <dkrakaksakst@gmail.com>
2026-08-24 21:05:36 +0900

    security: enforce upload policy on model saves

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class DashboardConfig(AppConfig):
-    name = 'dashboard'
+    name = "dashboard"
+
+    def ready(self):
+        from .upload_policy import register_model_upload_validation
+
+        register_model_upload_validation()
